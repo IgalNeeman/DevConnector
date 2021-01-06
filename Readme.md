f70ced3a1cdd75b984ccaa683827ccf3fc72b65b
IgalNeeman <44444912+IgalNeeman@users.noreply.github.com>
2021-01-06 11:25:20 +0200

    s

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Full Stack Project
+## כותרת משנה או עליונה
+* סעיפים
+## דוגמא לתמונה
+![Logo](https://github.com/IgalNeeman/drushim/blob/master/src/picsjobs/TNOVA-EXAMPLE.jpg)
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,3 +1,4 @@
+//part 10 Request & Body Validation
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
@@ -15,23 +16,21 @@ router.post(
   [
     check("name", "name is required").not().isEmpty(),
     check("email", "please include a vaild email").isEmail(),
-    check(
-      "password",
-      "please enter a password with 6 or more charchers"
-    ).isLength({ min: 6 }),
+    check("password","please enter a password with 6 or more charchers").isLength({ min: 6 }),
   ],
-  async (req, res) => {
+  async (req, res) => 
+  {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
+    if (!errors.isEmpty()) 
+    {
       return res.status(400).json({ errors: errors.array() });
     }
     const { name, email, password } = req.body;
     try {
       let user = await User.findOne({ email });
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "User already exists" }] });
+      if (user) 
+      {
+        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
       }
       const avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
       user = new User({ name, email, avatar, password });
@@ -51,7 +50,8 @@ router.post(
           res.json({ token });
         }
       );  // הוא מחק פה את ה res.send משהו בפרק  12
-    } catch (err) {
+    } 
+    catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
     }
